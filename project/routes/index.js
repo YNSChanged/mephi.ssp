56677d237543eba6bff558c861b06d1d4b0dcf0d
Yudin Nikita <yns.student@gmail.com>
2016-11-26 22:41:28 +0300

    up

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+function Prod(name, descr, pic, href, color, price){
+    this.name = name;
+    this.descr = descr;
+    this.pic = pic;
+    this.href = href;
+    this.color = color;
+    this.price = price;
+}
+
+var prodList = [new Prod("1","2","3")]; //3 топовых товара
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { topProdList: prodList });
 });
 
+router.get('/product/:category', function (req, res) {
+    //тут получение из базы данных соответствующей категории.
+    res.render('products', { category: req.params.category })
+});
 
 module.exports = router;
