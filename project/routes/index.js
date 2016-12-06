ac409e9882aadeb801696c4bae051afd00b1d8fd
Yudin Nikita <yns.student@gmail.com>
2016-12-06 18:12:35 +0300

    up

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,30 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-function Prod(name, descr, pic, href, color, price){
+function Prod(name, descr, href, color, price, mainImg, imgs){
     this.name = name;
-    this.descr = descr;
-    this.pic = pic;
+    this.description = descr;
     this.href = href;
     this.color = color;
     this.price = price;
-    this.mainImg = "";
-    this.img = ["1", "2", "3"];
+    this.mainImg = mainImg;
+    this.img = imgs;
 }
 
-var prodList = [new Prod("1","2","3")]; //3 топовых товара
+var prodList = [new Prod('Палатка МОБИ 2','Вместимость (человек): 2\nМатериалы каркаса: Fiberglass\nКонструкция: Дуговые\nТкань тента: Poly Taffeta 190T PU 2000\nТкань пола: Poly Taffeta 190T PU 3000\nПроклеенные швы тента\nВес (кг): 1,76\nВодостойкость тента (мм/в.ст.): 2 000\nВодостойкость дна (мм/в.ст.): 3 000','/product/id=1','Синий','1990','id1.jpg',['/product/id=1','/product/id=1','/product/id=1'])]; //3 топовых товара
 var prod = new Prod("1","2","3","4");
 
 router.get('/', function(req, res) {
-  res.render('index', { topProdList: prodList });
+    //получение из бд 3 топовых товаров.
+    res.render('index', { topProdList: prodList });
 }).get('/product/:category', function (req, res) {
     //тут получение из базы данных соответствующей категории.
     res.render('products', { topProdList: prodList });
-}).get('/single/:id', function (req, res) {
+}).get('/product/:id', function (req, res) {
     //запрос из бд
     res.render('single', {prod: prod, p: [prod, prod, prod]});
 }).get('/basket', function (req, res) {
-
 });
 
 module.exports = router;
